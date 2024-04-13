f6ef0c939db20c9e7913d6b7217c8b662e6c0d16
unstoppable840 <unstoppable840@gmail.com>
2024-04-13 21:11:50 +0800

    update: 添加了环境区分

--- src/redis/connect.ts
+++ src/redis/connect.ts
@@ -4,7 +4,7 @@ import {createPool} from 'generic-pool';
 // 创建 Redis 客户端
 const redisClient = createClient({
   url: 'redis://8.130.54.105:6379',
-  database: 1,
+  database: process.env.NODE_ENV==='dev'?1:2,
 }).on('error', err =>{ 
   console.log('Redis Client Error', err);},
 ).connect();
