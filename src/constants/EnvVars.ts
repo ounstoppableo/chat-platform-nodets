69f64d3fd8a67b572e69bddff4fd68dc38a9edc6
unstoppable840 <unstoppable840@gmail.com>
2024-04-14 14:18:36 +0800

    update: update

--- src/constants/EnvVars.ts
+++ src/constants/EnvVars.ts
@@ -7,7 +7,7 @@
 
 export default {
   NodeEnv: (process.env.NODE_ENV ?? ''),
-  Port: (process.env.PORT ?? 0),
+  Port: (4000),
   CookieProps: {
     Key: 'ExpressGeneratorTs',
     Secret: (process.env.COOKIE_SECRET ?? ''),
