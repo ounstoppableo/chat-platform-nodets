513cc1388f76f0aff030c9ba4311cb9c35ffcb17
unstoppable840 <unstoppable840@gmail.com>
2025-12-14 20:12:59 +0800

    modify: 打包优化

--- build.ts
+++ build.ts
@@ -6,7 +6,6 @@ import fs from 'fs-extra';
 import logger from 'jet-logger';
 import childProcess from 'child_process';
 
-
 /**
  * Start
  */
@@ -20,10 +19,13 @@ import childProcess from 'child_process';
     await copy('./tsconfig.json', './dist/tsconfig.json');
     await copy('./package.json', './dist/package.json');
     // Copy back-end files
-    await exec('tsc --build tsconfig.prod.json', './');
+    await exec(
+      'tsc --build tsconfig.prod.json && tsc-alias -p tsconfig.prod.json',
+      './',
+    );
   } catch (err) {
     logger.err(err);
-    process.exit(1);
+    throw new Error('打包失败');
   }
 })();
 
@@ -33,7 +35,7 @@ import childProcess from 'child_process';
 function remove(loc: string): Promise<void> {
   return new Promise((res, rej) => {
     return fs.remove(loc, (err) => {
-      return (!!err ? rej(err) : res());
+      return !!err ? rej(err) : res();
     });
   });
 }
@@ -44,7 +46,7 @@ function remove(loc: string): Promise<void> {
 function copy(src: string, dest: string): Promise<void> {
   return new Promise((res, rej) => {
     return fs.copy(src, dest, (err) => {
-      return (!!err ? rej(err) : res());
+      return !!err ? rej(err) : res();
     });
   });
 }
@@ -54,14 +56,14 @@ function copy(src: string, dest: string): Promise<void> {
  */
 function exec(cmd: string, loc: string): Promise<void> {
   return new Promise((res, rej) => {
-    return childProcess.exec(cmd, {cwd: loc}, (err, stdout, stderr) => {
+    return childProcess.exec(cmd, { cwd: loc }, (err, stdout, stderr) => {
       if (!!stdout) {
         logger.info(stdout);
       }
       if (!!stderr) {
         logger.warn(stderr);
       }
-      return (!!err ? rej(err) : res());
+      return !!err ? rej(err) : res();
     });
   });
 }
